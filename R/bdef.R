40afb052afeeef655dff0c91968781d6a76669e9
guiludwig <guilhermeludwig@gmail.com>
2016-12-20 02:00:08 -0200

    Updated documentation, as well as a optimization traceback option for debugging purposes.

--- R/bdef.R
+++ R/bdef.R
@@ -27,12 +27,20 @@
 #'                     parameter will be estimated. Otherwise, it will use
 #'                     the user supplied value. The parameter is in a log
 #'                     scale, thus ranging from -Inf to +Inf.
+#'  @param maxit The maximum number of iterations between
+#'                     optimization of the covariance parameters
+#'                     and estimation of the spline coefficients.
+#'                     Defaults to 2.
+#'  @param traceback Whether the function will return each step
+#'                     of the optimization iterations, as a list.
+#'                     For debugging purposes. Defaults to FALSE.
 #'  @param ... Additional arguments for RFfit.
 #'
 #' @export
 #' @return \code{bdef} returns an object of \code{\link{class}} "bdef"
 #' containing at least the following components
-#'   \item{window}{The w}
+#'   \item{window}{The rectangular spatial domain in which the
+#'                data was sampled}
 #'   \item{basis}{list of B-spline basis functions, inherited from
 #'                \code{\link{bs}}, with one set for each coordinate}
 #'   \item{theta1}{B-spline coefficients for the coordinate x1}
@@ -46,21 +54,21 @@
 #' x2 <- runif(n)
 #' x <- cbind(x1,x2)
 #' F1 <- function(x1,x2) {
-#'   (x1 - 2*x2)^2/sqrt(x1^2 + 4*x2^2)
+#'   x1 # (x1 - 2*x2)^2/sqrt(x1^2 + 4*x2^2)
 #' }
 #' F2 <- function(x1,x2) {
-#'   (3*x1 + x2)^2/sqrt(9*x1^2 + x2^2)
+#'   sqrt(x1*x2) # (3*x1 + x2)^2/sqrt(9*x1^2 + x2^2)
 #' }
 #' plotGrid(list(window = list(x = range(x1), y = range(x2)),
 #'               x = x, def.x = cbind(F1(x1,x2), F2(x1,x2))),
 #'          F1 = F1, F2 = F2, margins = TRUE)
-#' covModel <- RMmatern(nu = 2.5, var = 1, scale = 2) + RMnugget(var = .01)
+#' covModel <- RMmatern(nu = 2.5, var = 1, scale = 2) # No nugget
 #' data <- RFsimulate(covModel, x = F1(x1,x2), y = F2(x1,x2))
 #' y <- as.numeric(unlist(data@data))
 #' test1 <- bdef(x, y) # lambda estimated
 #' test2 <- bdef(x, y, lambda = -100) # lambda provided by user
-#' test3 <- bdef(x, y, lambda = 100) # lambda provided by user
-#' test4 <- bdef(x, y, cov.model = RMmatern(nu = 2.5, var = NA, scale = NA)) # No nugget effect (default)
+#' test3 <- bdef(x, y, lambda = 100) # ditto
+#' test4 <- bdef(x, y, cov.model = RMmatern(nu = 2.5, var = NA, scale = NA)) # No nugget effect
 #' plotGrid(test1, margins = TRUE)
 #' plotGrid(test2, margins = TRUE)
 #' plotGrid(test3, margins = TRUE)
@@ -79,7 +87,8 @@ bdef <- function(x, y,
                  cov.model = RMmatern(nu = 2.5, var = NA, scale = NA) + RMnugget(var = NA),
                  df1 = 6, df2 = 6,
                  window = list(x = range(x[,1]), y = range(x[,2])),
-                 lambda = NA, maxit = 2, ...) {
+                 lambda = NA, maxit = 2,
+                 traceback = FALSE, ...) {
 
   RFoptions(printlevel=0, warn_normal_mode=FALSE)
 
@@ -96,12 +105,6 @@ bdef <- function(x, y,
   theta00 <- c(as.numeric(solve(crossprod(W) + .001*diag(df1*df2)/max(W), crossprod(W, x[, 1]))),
                as.numeric(solve(crossprod(W) + .001*diag(df1*df2)/max(W), crossprod(W, x[, 2]))))
 
-  # model fitting here: for k = 1, 2, ...
-  # k: with theta1, theta2, estimate mu(?), sigma,
-  #    rho, (kappa fixed?); use maybe RandomFields::RFfit?
-  # k+1: with mu, sigma, rho, kapp, estimate theta1, theta2
-  # theta0 ~ identity?
-
   model0 <- try(RFfit(model = cov.model, x = x[,1], y = x[,2], data = y))
 
   # bsplinedef:::likelihoodTarget(c(seq(1, 2*df1*df2), 1))
@@ -114,6 +117,14 @@ bdef <- function(x, y,
     theta0 <- optim(theta00, likelihoodTarget, # seq(1, 2*df1*df2)
                     DF1 = df1, DF2 = df2, B = basis,
                     M = model0, X = x, Y = y, L = lambda)$par
+  }
+
+  # Traces the deformation map estimation
+  if(traceback) {
+    trace <- list()
+    trace[["model_0"]] <- model0
+    trace[["theta_0"]] <- list(theta1 = theta0[1:(df1*df2)],
+                               theta2 = theta0[1:(df1*df2) + (df1*df2)])
   }
 
   f1 <- as.numeric(W%*%theta0[1:(df1*df2)])
@@ -129,6 +140,13 @@ bdef <- function(x, y,
                      DF1 = df1, DF2 = df2, B = basis,
                      M = model1, X = x, Y = y, L = lambda)$par
 
+  # Traces the deformation map estimation
+  if(traceback) {
+    trace[["model_1"]] <- model1
+    trace[["theta_1"]] <- list(theta1 = theta.new[1:(df1*df2)],
+                               theta2 = theta.new[1:(df1*df2) + (df1*df2)])
+  }
+
   it <- 1
   condition <- TRUE
   while(it < maxit & condition){
@@ -142,6 +160,12 @@ bdef <- function(x, y,
                        M = model1, X = x, Y = y, L = lambda)$par
     condition <- all(max(abs(theta0 - theta.new)/theta0, 0.0009) < 0.001)
     it <- it + 1
+    # Traces the deformation map estimation
+    if(traceback) {
+      trace[[paste0("model_", it)]] <- model1
+      trace[[paste0("theta_", it)]] <- list(theta1 = theta.new[1:(df1*df2)],
+                                            theta2 = theta.new[1:(df1*df2) + (df1*df2)])
+    }
   }
 
   ret <- list(window = window,
@@ -153,6 +177,10 @@ bdef <- function(x, y,
               df1 = df1,
               df2 = df2,
               model = model1)
+  # Traces the deformation map estimation
+  if(traceback){
+    ret$trace <- trace
+  }
   class(ret) <- "bdef"
 
   RFoptions(printlevel=1, warn_normal_mode=TRUE)
